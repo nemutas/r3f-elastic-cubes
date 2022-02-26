dcbe46dcba864d0060f9bbc53a907f7da519011e
nemutas <inohiro.focru@gmail.com>
2022-02-26 17:15:04 +0900

    add repository link button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { VFC } from 'react';
 import { css } from '@emotion/css';
+import { LinkIconButton } from './LinkIconButton';
 import { TCanvas } from './three/TCanvas';
 
 export const App: VFC = () => {
 	return (
 		<div className={styles.container}>
 			<TCanvas />
+			<LinkIconButton imagePath="/assets/icons/github.svg" linkPath="https://github.com/nemutas/r3f-elastic-cubes" />
 		</div>
 	)
 }
